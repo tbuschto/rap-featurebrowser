1a833633bce345b891900c48b380b9674d638a98
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-07-23 17:38:20 +0200

    Add help button to header that does nothing

--- org.eclipse.rap.featurebrowser/src/org/eclipse/rap/featurebrowser/EntryPoint.java
+++ org.eclipse.rap.featurebrowser/src/org/eclipse/rap/featurebrowser/EntryPoint.java
@@ -8,8 +8,11 @@ import org.eclipse.rap.json.JsonArray;
 import org.eclipse.rap.rwt.RWT;
 import org.eclipse.rap.rwt.application.AbstractEntryPoint;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 
 public class EntryPoint extends AbstractEntryPoint {
@@ -17,14 +20,7 @@ public class EntryPoint extends AbstractEntryPoint {
     @Override
     protected void createContents( Composite parent ) {
       parent.setLayout( LayoutUtil.createVerticalLayout() );
-      CLabel header = new CLabel( parent, SWT.NONE );
-      header.setText( ">>  REMOTE APPLICATION PLATFORM FEATURE BROWSER" );
-      header.setMargins( 16, 16, 30, 10 );
-      header.setData( RWT.CUSTOM_VARIANT, "header" );
-      header.setLayoutData( LayoutUtil.createVerticalLayoutData( 45 ) );
-      Composite subheader = new Composite( parent, SWT.NONE );
-      subheader.setData( RWT.CUSTOM_VARIANT, "subheader" );
-      subheader.setLayoutData( LayoutUtil.createVerticalLayoutData( LayoutUtil.BARWIDTH ) );
+      createHeader( parent );
       Composite main = LayoutUtil.createHorizontalComposite( parent, 2 );
       main.setLayoutData( LayoutUtil.createFillData() );
       InputStream resource = getClass().getClassLoader().getResourceAsStream( "features.json" );
@@ -45,6 +41,25 @@ public class EntryPoint extends AbstractEntryPoint {
       } else {
         throw new RuntimeException( "Could not read features" );
       }
+    }
+
+    public void createHeader( Composite parent ) {
+      Composite header = new Composite( parent, SWT.NONE );
+      header.setData( RWT.CUSTOM_VARIANT, "header" );
+      header.setLayoutData( LayoutUtil.createVerticalLayoutData( 45 ) );
+      header.setLayout( new GridLayout( 2, false ) );
+      header.setBackgroundMode( SWT.INHERIT_FORCE );
+      Label headerLabel = new Label( header, SWT.NONE );
+      headerLabel.setText( ">>  REMOTE APPLICATION PLATFORM FEATURE BROWSER" );
+      headerLabel.setData( RWT.CUSTOM_VARIANT, "headerLabel" );
+      headerLabel.setLayoutData( new GridData( SWT.LEFT, SWT.CENTER, true, true ) );
+      Button help = new Button( header, SWT.PUSH );
+      help.setText( "Getting started..." );
+      help.setLayoutData( new GridData( SWT.RIGHT, SWT.BOTTOM, false, true ) );
+      help.setData( RWT.CUSTOM_VARIANT, "helpButton" );
+      Composite subheader = new Composite( parent, SWT.NONE );
+      subheader.setData( RWT.CUSTOM_VARIANT, "subheader" );
+      subheader.setLayoutData( LayoutUtil.createVerticalLayoutData( LayoutUtil.BARWIDTH ) );
     }
 
 }
