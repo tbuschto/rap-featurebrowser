839ec3ff3d65d80c18aaad3bd312d4c7d9e52579
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-07-26 00:42:26 +0200

    Show a placeholder help text

--- org.eclipse.rap.featurebrowser/src/org/eclipse/rap/featurebrowser/Help.java
+++ org.eclipse.rap.featurebrowser/src/org/eclipse/rap/featurebrowser/Help.java
@@ -14,26 +14,31 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.service.ResourceManager;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
 
 public class Help {
 
-
   public Help( Shell parentShell ) {
+    ensureHtml();
     final Shell shell = new Shell( parentShell, SWT.CLOSE | SWT.TOP | SWT.APPLICATION_MODAL );
     Button close = new Button( shell, SWT.PUSH );
     shell.setFullScreen( true );
-    //shell.setLayout( LayoutUtil.createVerticalLayout( 30 ) );
     shell.setLayout( new FormLayout() );
     shell.setData( RWT.CUSTOM_VARIANT, "helpShell" );
     shell.addListener( SWT.Hide, new Listener() {
@@ -42,7 +47,6 @@ public class Help {
       }
     } );
     Composite area = new Composite( shell, SWT.BORDER );
-    //area.setLayoutData( LayoutUtil.createFillData() );
     FormData formData = new FormData();
     formData.left = new FormAttachment( 0, 35 );
     formData.top = new FormAttachment( 0, 35 );
@@ -50,8 +54,28 @@ public class Help {
     formData.bottom = new FormAttachment( 100, -35 );
     area.setLayoutData( formData );
     area.setData( RWT.CUSTOM_VARIANT, "helpBox" );
+    GridLayout layout = new GridLayout( 1, true );
+    layout.marginWidth = 15;
+    layout.marginTop = 8;
+    layout.marginBottom = -5; // WTF?
+    layout.verticalSpacing = 10;
+    area.setLayout( layout );
+    Label heading = new Label( area, SWT.NONE );
+    heading.setText( "Getting Started..." );
+    heading.setLayoutData( new GridData( SWT.CENTER, SWT.CENTER, true, false ) );
+    heading.setFont( new Font( heading.getDisplay(), "Segoe UI, Arimo, Calibri, Verdana, Sans-serif", 36, SWT.ITALIC ) );
+    heading.setData( RWT.CUSTOM_VARIANT, "helpHeading" );
+    Browser browser = new Browser( area, SWT.NONE );
+    browser.setLayoutData( LayoutUtil.createFillData() );
+    browser.setUrl( RWT.getResourceManager().getLocation( "help.html" ) );
     InputStream stream = getClass().getResourceAsStream( "/resources/x-mark-4-xl.png" );
     close.setImage( new Image( close.getDisplay(), stream ) );
+    try {
+      stream.close();
+    } catch( IOException e1 ) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
     close.setData( RWT.CUSTOM_VARIANT, "helpClose" );
     close.addListener( SWT.Selection, new Listener() {
       public void handleEvent( Event event ) {
@@ -68,6 +92,20 @@ public class Help {
     formData.right = new FormAttachment( 100, -10 );
     close.setLayoutData( formData );
     shell.open();
+  }
+
+  private static void ensureHtml() {
+    ResourceManager resourceManager = RWT.getResourceManager();
+    if( !resourceManager.isRegistered( "help.html" ) ) {
+      InputStream stream
+        = Help.class.getClassLoader().getResourceAsStream( "/resources/help.html" );
+      resourceManager.register( "help.html", stream );
+      try {
+        stream.close();
+      } catch( IOException e ) {
+        e.printStackTrace();
+      }
+    }
   }
 
 }
